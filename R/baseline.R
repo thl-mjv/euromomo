00f1f490edaefbabbbaa11253478e01ba8e7879e
kontot <achillestsoumanis@hotmail.com>
2014-11-24 19:15:42 +0100

    Add conditions and create week variables

--- R/baseline.R
+++ R/baseline.R
@@ -25,24 +25,20 @@
 #' @value EuroMOMO data with predicted values, prediction variances and overdispersion
 #' @export
 baseline <- function(data, seasonality =TRUE, ...){
-
-  # Sort the data
-  data <- data[order(data$YWoDi), ]
-
   # STEP 1: Calculate the trend (YWoDi as continuous)
-  data$wk <- c(1:nrow(data))
+  data<-addweeks(data)
 
   # STEP 2: Calculate the sin-cos trends
-  data$sin1 <- sin(2*pi*wk/52.18)
-  data$cos1 <- cos(2*pi*wk/52.18)
+  data$sin1 <- sin(2*pi * data$wk/52.18)
+  data$cos1 <- cos(2*pi * data$wk/52.18)
 
   # STEP 3: Fit the Poisson model (linear trend) only when the conditions apply
   # Create dummy variable combining all conditions
-  data$cond <- with(data,ifelse(COnd3==1 & Cond4==1 & Cond5==1 & Cond6==1, 1, 0))
+  data$cond <- with(data,ifelse(Cond3==1 & Cond4==1 & Cond5==1 & Cond6==1, 1, 0))
   ### See the number of deaths on the period of interest. If too few or none, fitting is not possible
 
   glm_form <- "cnb ~wk"
-  if(seasonality==TRUE) update(glm_form, paste(glm_form,"+ sin1 + cos1"))
+  if(seasonality==TRUE) glm_form<-paste(glm_form,"+ sin1 + cos1")
 
   fit <- try(glm(formula(glm_form), data = data[data$cond==1, ], family = quasipoisson()))
 
@@ -61,5 +57,28 @@ baseline <- function(data, seasonality =TRUE, ...){
   data$overdispersion <- summary(fit)$dispersion
   }
 
+  return(data)
+}
+
+addconditions <- function(data, spring=15:26, autumn=36:45, duration=5*52, last=NULL, delay=0){
+  data<-addweeks(data)
+  data$Cond3 <- ifelse(with(data, WoDi %in% c(spring, autumn)), 1, 0)
+  if(is.null(last))
+    data$Cond4 <- rep(1,nrow(data))
+
+  data$Cond5<-with(data,ifelse(wk<max(wk)-delay,1,0))
+
+  data$Cond6 <- with(data,ifelse(wk > max(wk)-duration - delay, 1, 0))
+
+  return(data)
+
+}
+
+addweeks<-function(data){
+  data$YWoDi<-with(data,sprintf("%04d-%02d",YoDi,WoDi))
+  # Sort the data
+  data <- data[order(data$YWoDi), ]
+
+  data$wk <- c(1:nrow(data))
   return(data)
 }
