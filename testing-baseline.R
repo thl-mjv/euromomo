691a5304f84f781892c35a23c3439e5f7388cca1
Mikko J Virtanen <mikko.virtanen@thl.fi>
2014-11-25 11:11:46 +0200

    Adding spline bases

--- testing-baseline.R
+++ testing-baseline.R
@@ -1,12 +1,28 @@
 
 data <- read.csv2("delay.txt")
 
-data2<-addconditions(data)
+data2<-addconditions(data,spring=5:26,autumn=30:48)
 
 
-data3 <- baseline(data2)
-traceback()
+data3 <- baseline(data2,seasonality=2,spline="l",splinedf=2)
+
+with(data3,matplot(cbind(sin1,sin2),type="l"))
+
+data4 <- zscore(data3)
+
+data5 <- excess(data4)
+
+head(data5)
 
 library("lattice")
 
-xyplot(nb+cnb+pnb~wk,data=data3,type="l")
+xyplot(nb+cnb+pnb+u.pnb+l.pnb~wk,data=data5,type="l")
+xyplot(Zscore~wk,data=data5,type="l")
+xyplot(excess+u.excess+l.excess~wk,data=data5,type="l")
+
+#### the problem with spines
+x1<-1:200
+x2<-1:250
+library(splines)
+matplot(x1,bs(x1),type="l",xlim=c(1,250))
+matlines(x2,bs(x2),type="l")
