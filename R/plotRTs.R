395d889694703af26217182d54548384dcccbdde
hoehleatsu <hoehle@lillagubben>
2015-02-09 23:57:53 +0000

    Delay diagnostic plots are now pngs. Furthermore, the legend of the first plot is now outside the plot (Stata style), which is better in case many many different delays are plotted.

--- R/plotRTs.R
+++ R/plotRTs.R
@@ -16,20 +16,39 @@ verticalLineSDE <- function(mondays) {
 
 #' Show the delay as a function of time.
 #'
-#' @param df a data frame representing a reporting triangle.
+#' @param df a data frame representing a reporting triangle (Note: it's cumulative).
 #' @param main Title of the plot (typically the label of the agegroup)
 #' @return nothing (NULL)
 #' @export
 plotDelay <- function(df, main=NULL) {
+  #Get the index of the delay columns (note: the rt is cumulative)
   delayIdx <-  grep("^w[0-9]+$",colnames(df))
   maxDelay <- length(delayIdx) - 1
+  #Totals - note: we only take complete weeks, because otherwise it's NA.
   total <- df[,max(delayIdx)]
 
   mondays <- ISOweek2date(paste0(df$ISOweek,"-1"))
+
+  #Someone prefers the Stata way of placing legends.
+  layout(mat = matrix(c(1, 2), nrow = 2, ncol = 1), heights = c(4, 1))
+
   #Matplot doesn't handle dates in the x-axis formatting. Using plot followed matlines
   plot(mondays,rep(0,length(mondays)),ylab="Proportion of total",xlab="Time of death",ylim=c(0,1),main=main,type="n")
-  matlines(mondays, df[,delayIdx]/matrix(total,nrow=nrow(df),ncol=maxDelay+1,byrow=FALSE),type="l",lty=1)
+  cdfDelay <- df[,delayIdx]/matrix(total,nrow=nrow(df),ncol=maxDelay+1,byrow=FALSE)
+  matlines(mondays, cdfDelay,type="l",lty=1)
+  #Add a line indicating the start of the delay estimation (SDE)
   verticalLineSDE(mondays)
+
+  #Also indicate last complete week
+  lastCompleteIdx <- which.min(!is.na(total))
+  lines( rep(mondays[lastCompleteIdx],2), c(0,1e99),lwd=2,lty=2)
+  axis(3, at=mondays[lastCompleteIdx], labels=paste0("Last complete\n(when DMax=",maxDelay,")"),cex.axis=0.7,lwd=2,las=1)
+
+
+  #Add a legend (in seperate layout pane)
+  par(mar = rep(0, 4))
+  plot.new()
+  plot.window(xlim = c(0, 1), ylim = c(0, 1))
   legend(x="bottom", ncol=5, paste0(0:maxDelay," weeks"), lty=1,col=seq_len(maxDelay+1),bg="white")
 
   invisible(NULL)
@@ -61,6 +80,7 @@ plotDelayQuantiles <- function(rT, w=1, ISOweeks, quantiles=c(0.1,0.5,0.9),col=1
   #Determine max delay from the reporting triangle.
   delayIdx <-  grep("^w[0-9]+$",colnames(rT))
   maxDelay <- length(delayIdx) - 1
+
   #Initialize time varying PMF data structure
   res <- matrix(NA, nrow=length(ISOweeks), ncol=maxDelay+1)
 
@@ -93,9 +113,7 @@ plotDelayQuantiles <- function(rT, w=1, ISOweeks, quantiles=c(0.1,0.5,0.9),col=1
   plot(mondays, quants[,1],xlab="Time of death",ylab="Reporting Delay (weeks)",ylim=c(0,maxDelay),type="l",col=col[1],lty=lty[1],lwd=lwd[1],main=main)
   #Add grid for easier reading.
   grid(ny=NULL,nx=NA,lwd=2)
-  #void <- sapply(seq(0,maxDelay,by=2), function(i) {
-  #  abline(a=i,b=0,col="lightgray",lty=2)
-  #})
+
   axis(2,at=0:maxDelay,labels=FALSE,tcl=-0.25)
   #Add additional lines.
   if (length(quantiles)>1) {
@@ -104,6 +122,11 @@ plotDelayQuantiles <- function(rT, w=1, ISOweeks, quantiles=c(0.1,0.5,0.9),col=1
   #Draw vertical line indicating start of delay estimation.
   verticalLineSDE(mondays)
 
+  #Also indicate last complete week
+  total <- rT[,max(delayIdx)]
+  lastCompleteIdx <- which.min(!is.na(total))
+  lines( rep(mondays[lastCompleteIdx],2), c(0,1e99),lwd=2,lty=2)
+  axis(3, at=mondays[lastCompleteIdx], labels=paste0("Last complete\n(when DMax=",maxDelay,")"),cex.axis=0.7,lwd=2,las=1)
 
   #Make a legend
   expressions <- lapply(quantiles, function(quantile) substitute(expression(q[x]),list(x=quantile)))
@@ -123,18 +146,30 @@ plotDelayQuantiles <- function(rT, w=1, ISOweeks, quantiles=c(0.1,0.5,0.9),col=1
 #' @export
 plotDelayDiagnostics2File <- function(rTList, w=1, quantiles=c(0.25,0.50,0.75,0.9,0.95,0.99), main=NULL, week.dir) {
   #Setup filename and open pdf device (could have been png also)
-  fileName <- paste0("Delay-", groupOpts["label"], ".pdf")
-  pdf(file = file.path(week.dir, "diagnostics", fileName), onefile=TRUE, width=8,height=6)
+  extension <- "png"
+  plotNames <- c("cumulative","quantiles")
+  fileNames <- paste0("Delay-", groupOpts["label"],"-",plotNames, ".",extension)
+  #Dimension of the images (in inch) + resolution
+  dim <- c(8,6)
+  resolution <- 300
 
-  #Call the two plot routines.-
+  #For reports, vector graphics would be better:
+  #pdf(file = file.path(weekdir, "diagnostics", fileName),onefile=TRUE, width=8, height=6)
+
+  #Call the plot routine generating the cumulative counts.
+  png(filename=file.path(week.dir, "diagnostics", fileNames[1]), width=dim[1], height=dim[2],units="in",res=resolution)
   plotDelay(rT2DataFrame(rTList$cumRT), main=main)
+  dev.off()
+
+  #Call the plot routine creating the plot of the delay quantiles
+  png(filename=file.path(week.dir, "diagnostics", fileNames[2]), width=dim[1], height=dim[2],units="in",res=resolution)
   rTDF <- rT2DataFrame(rTList$rT)
   plotDelayQuantiles(rTDF, w=w, ISOweeks=rTDF$ISOweek,
                      quantiles=quantiles,
                      lty=seq_len(length(quantiles)),
                      lwd=(seq_len(length(quantiles))+1) %/% 2,
                      col=seq_len(length(quantiles)),main=main)
-
-  #Close device
   dev.off()
+
+  invisible()
 }
